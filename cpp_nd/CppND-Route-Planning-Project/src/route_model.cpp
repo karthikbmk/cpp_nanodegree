26209a64c57b468ae0586414e5667173779513ec
karthikbmk <karthikbm2007@gmail.com>
2019-05-22 18:40:13 +0000

    extending openstreet map model

--- cpp_nd/CppND-Route-Planning-Project/src/route_model.cpp
+++ cpp_nd/CppND-Route-Planning-Project/src/route_model.cpp
@@ -1,6 +1,84 @@
 #include "route_model.h"
 #include <iostream>
+#include <vector>
+#include <limits>
+
+using std::vector;
+using std::numeric_limits;
+
 
 RouteModel::RouteModel(const std::vector<std::byte> &xml) : Model(xml) {
+    vector <Model::Node> nodes = this->Nodes();
+    int i = 0;
+    for(Model::Node node : nodes){
+        RouteModel::Node rm_node = RouteModel::Node(i, this, node);
+        m_Nodes.push_back(rm_node);
+        i+=1;
+    }
+
+}
+
+void RouteModel::CreateNodeToRoadHashMap(){
+    for(Road road : Roads()){
+        if (road.type != Model::Road::Type::Footway){
+            for (int node_idx : Ways()[road.way].nodes){
+                 if (node_to_road.find(node_idx) == node_to_road.end()){
+                    node_to_road[node_idx] = vector <const Model::Road*> ();
+                }
+                node_to_road[node_idx].push_back(&road);
+            }
+        }
+    }
+}
+
+RouteModel::Node* RouteModel::Node::FindNeighbor(vector<int> node_indices){
+    float closest_dist = numeric_limits<float>::max();
+    RouteModel::Node *closest_node = nullptr;
+    for(int node_idx : node_indices){
+        RouteModel::Node tmp_node = parent_model->SNodes()[node_idx];
+        bool is_visited = tmp_node.visited;
+        float dist = RouteModel::Node::distance(tmp_node);
+        if (!is_visited && dist > 0){
+            if (dist < closest_dist){
+                closest_dist = dist;
+                closest_node = &tmp_node;
+            }
+        }
+    }
+    return closest_node;
+}
+
+void RouteModel::Node::FindNeighbors(){
+    vector<const Model::Road*> road_ptrs = parent_model->node_to_road[this->index];
+    for(const Road* road_ptr : road_ptrs){
+        vector <int> nodes = parent_model->Ways()[road_ptr->way].nodes;
+        RouteModel::Node *closest_node = FindNeighbor(nodes);
+        if (closest_node != nullptr){
+            this->neighbors.push_back(closest_node);
+        }
+    }
+}
+
+RouteModel::Node & RouteModel::FindClosestNode(float x, float y){
+    RouteModel::Node user_node;
+    user_node.x = x;
+    user_node.y = y;
+
+    float min_dist = numeric_limits<float>::max();
+    int closest_idx = -1;
+
+    for (Road road : Roads()){
+        if (road.type != Model::Road::Type::Footway){
+            for (int node_idx : Ways()[road.way].nodes){
+                float tmp_dist = user_node.distance(SNodes()[node_idx]);
+                if (tmp_dist  < min_dist){
+                    min_dist = tmp_dist;
+                    closest_idx = node_idx;
+                }
+
+            }
+        }
+    }
+    return SNodes()[closest_idx];
 
 }
--- cpp_nd/CppND-Route-Planning-Project/src/route_model.h
+++ cpp_nd/CppND-Route-Planning-Project/src/route_model.h
@@ -6,27 +6,44 @@
 #include "model.h"
 #include <iostream>
 
+using std::vector;
+using std::unordered_map;
+
 class RouteModel : public Model {
 
   public:
     class Node : public Model::Node {
       public:
         // Add public Node variables and methods here.
+        Node *parent = nullptr;
+        float h_value =  std::numeric_limits<float>::max();
+        float g_value = 0.0;
+        bool visited = false;
+        vector <Node *> neighbors;
         
         Node(){}
         Node(int idx, RouteModel * search_model, Model::Node node) : Model::Node(node), parent_model(search_model), index(idx) {}
+        float distance(Node node) const{return sqrt(pow((this->x - node.x),2) + pow((this->y - node.y),2));}
+        void FindNeighbors();
       
       private:
         // Add private Node variables and methods here.
         int index;
         RouteModel * parent_model = nullptr;
+        RouteModel::Node* FindNeighbor(vector<int> node_indices);
     };
     
     // Add public RouteModel variables and methods here.
     RouteModel(const std::vector<std::byte> &xml);  
     std::vector<Node> path; // This variable will eventually store the path that is found by the A* search.
+    vector<Node> & SNodes(){return m_Nodes;}
+    RouteModel::Node &FindClosestNode(float x, float y);
 
   private:
     // Add private RouteModel variables and methods here.
+    vector<Node> m_Nodes;
+    unordered_map <int, vector<const Model::Road*>> node_to_road;
+    void CreateNodeToRoadHashMap();
+
 
 };
